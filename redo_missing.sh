0c24258625e6d163a61bf9fc9a90c4bd2cce9198
AWS ParallelCluster user <ec2-user@ip-10-0-12-184.ap-southeast-1.compute.internal>
2021-11-08 05:40:48 +0000

    update files from lab 8

--- redo_missing.sh
+++ redo_missing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cwd=`pwd`; 
-for i in data/picasso/20181102/session01/array04/channel118;
+for i in data/picasso/20181105/session01/array01/channel027;
 do echo $i;
 	cd /$i;
 	sbatch /data/src/PyHipp/rplhighpass-sort-slurm.sh; cd $cwd; done
